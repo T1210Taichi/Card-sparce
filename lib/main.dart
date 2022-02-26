115bcc2aa6f77345852cdb718d8af6c4efdf6564
T1210Taichi <T1210Taichi@gmail.com>
2022-02-26 12:29:02 +0900

    2 card

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const appName = "Card Space";
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  static const String appName = "Card Space";
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Offset pos = Offset(0, 0);
+  Offset pos1 = Offset(0, 0);
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      backgroundColor: Colors.grey[100], //背景色
       body: Stack(
         //Widgetを自由に配置
         children: <Widget>[
@@ -52,7 +54,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 //移動中のWidget
                 width: 200,
                 height: 100,
-                color: Colors.blue,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: const Center(
                   child: Text(
                     "test",
@@ -64,7 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 //移動する前のWidget
                 width: 200,
                 height: 100,
-                color: Colors.blue,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: const Center(
                   child: Text(
                     "test",
@@ -78,6 +88,54 @@ class _MyHomePageState extends State<MyHomePage> {
                 setState(() {
                   //Widgetの位置を更新
                   pos = Offset(offset.dx, offset.dy - 56);
+                });
+              },
+            ),
+          ),
+          Positioned(
+            //位置を指定
+            left: pos1.dx,
+            top: pos1.dy,
+            child: Draggable(
+              //ドラッグで移動可能に
+              feedback: Container(
+                //移動中のWidget
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "test",
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
+                  ),
+                ),
+              ),
+              child: Container(
+                //移動する前のWidget
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "test2",
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
+                  ),
+                ),
+              ),
+              childWhenDragging: Container(), //移動中の移動する前のWidget
+              onDraggableCanceled: (view, offset) {
+                //ドラッグを離した処理
+                setState(() {
+                  //Widgetの位置を更新
+                  pos1 = Offset(offset.dx, offset.dy - 56);
                 });
               },
             ),
